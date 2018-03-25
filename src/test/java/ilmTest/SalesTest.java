71e36379985c3793b1b9c47cabe4305dc04b77c0
Tzeeka <Tzeeka@gmail.com>
2018-03-25 20:33:52 +0500

    Добавлен assertEquals, проверяющий есть ли пометка "Скидка" на карточке товаров.

--- src/test/java/ilmTest/SalesTest.java
+++ src/test/java/ilmTest/SalesTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 import java.util.List;
 
 public class SalesTest extends baseTest {
-    //Проверяем наличие значка "Скидка" у всех товаров на странице, выводим текст значка каждого товара в консоль
+    //Проверяем наличие значка "Скидка" у всех товаров на странице
     @Test
     public void testIlm() {
         driver.get("https://ilovemum.ru/catalog/special_filter/SPECIALOFFER/");
